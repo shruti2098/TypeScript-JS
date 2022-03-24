e19aa3395e0c7cbc442bb9f240f52f58dbca6a3b
shruti2098 <shrutiiadam2098@gmail.com>
2022-03-24 11:14:36 +0530

    files added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,35 +1,7 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailsComponent } from './details/details.component';
-import { EditComponent } from './edit/edit.component';
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
-import { UserdetailsComponent } from './userdetails/userdetails.component';
 
-const routes: Routes = [
-  {
-    path:"",
-    component:UserdetailsComponent,
-  },
-  {
-    path:"userdetails",
-    component:UserdetailsComponent,
-  },
-  {
-    path:"edit/:id",
-    component:EditComponent,
-  },
-  {
-    path:"details/:id",
-    component:DetailsComponent,
-  },
-  {
-    path:'userdetails' , loadChildren: () => import('./userdetails/userdetails.module').then(m => m.UserdetailsModule)
-  },
-  {
-    path:"**",
-    component:PagenotfoundComponent,
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
